e4c4267a897f88d48c38e5a95f3a1d4012026fcb
Bidhan <bidhanbiswas.cse@gmail.com>
2022-04-16 04:26:37 -0500

    First commit

---
+++ IDTech/src/com/bidhan/interview/ClassDemoCar.java
@@ -0,0 +1,44 @@
+package com.bidhan.interview;
+
+public class ClassDemoCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Car myCar=new Car();
+		
+		myCar.brand="BMW";
+		myCar.color="White";
+		myCar.price=10000;
+		
+		myCar.details();
+		myCar.start();
+		myCar.drive();
+		myCar.stop();
+	}
+	
+	public static class Car{
+
+		String brand;
+		String color;
+		int price;
+		
+		void details() {
+			System.out.println("Brand: "+brand+", Color: "+color+", Price: "+price);
+		}
+		
+		void start() {
+			System.out.println("Car has started!!!");
+		}
+		
+		void drive() {
+			System.out.println("Car is running!!!");
+		}
+		
+		void stop() {
+			System.out.println("Car has stopped!!!");
+		}
+		
+	}
+
+}
